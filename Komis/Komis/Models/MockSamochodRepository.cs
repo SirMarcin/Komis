1a3c7a3183533b28f2654647340f99aa1fcabcf0
Marcin Buczyński <buczynski.programuje@gmail.com>
2018-11-12 18:17:33 +0100

    poprawienie widoku

--- Komis/Komis/Models/MockSamochodRepository.cs
+++ Komis/Komis/Models/MockSamochodRepository.cs
@@ -19,10 +19,15 @@ namespace Komis.Models
         {
             samochody = new List<Samochod>
             {
-                new Samochod {Id=1, Cena=12500, JestSamochodemTygodnia=false, Marka="BMW", MiniaturkaUrl="", Moc= "150KM", Model="E46", Opis="Niemiec płakał jak sprzedawał", Pojemnosc="2,5l", Przebieg="220000km", RodzajPaliwa="Benzyna", RokProdukcji=2002, ZdjecieUrl=""  },
-                new Samochod {Id=2, Cena=7500, JestSamochodemTygodnia=true, Marka="Ford", MiniaturkaUrl="", Moc= "110KM", Model="Mondeo", Opis="Żabojad płakał jak sprzedawał", Pojemnosc="2,5l", Przebieg="180000km", RodzajPaliwa="Benzyna + Gaz", RokProdukcji=1999, ZdjecieUrl=""  },
-                new Samochod {Id=3, Cena=20000, JestSamochodemTygodnia=false, Marka="Mercedes", MiniaturkaUrl="", Moc= "205KM", Model="CLK200", Opis="Znowu Niemiec płakał jak sprzedawał", Pojemnosc="2,5l", Przebieg="240000km", RodzajPaliwa="Diesel", RokProdukcji=2007, ZdjecieUrl=""  }
-
+                new Samochod {Id=1, Cena=12500, JestSamochodemTygodnia=false, Marka="BMW", MiniaturkaUrl="/images/e46.jpg", Moc= "150KM", Model="E46", Opis="Niemiec płakał jak sprzedawał", Pojemnosc="2,5l", Przebieg="220000km", RodzajPaliwa="Benzyna", RokProdukcji=2002, ZdjecieUrl=""  },
+                new Samochod {Id=2, Cena=7500, JestSamochodemTygodnia=true, Marka="Ford", MiniaturkaUrl="/images/mondeo.jpg", Moc= "110KM", Model="Mondeo", Opis="Żabojad płakał jak sprzedawał", Pojemnosc="2,5l", Przebieg="180000km", RodzajPaliwa="Benzyna + Gaz", RokProdukcji=1999, ZdjecieUrl=""  },
+                new Samochod {Id=3, Cena=20000, JestSamochodemTygodnia=false, Marka="Mercedes", MiniaturkaUrl="/images/clk200.jpg", Moc= "205KM", Model="CLK200", Opis="Znowu Niemiec płakał jak sprzedawał", Pojemnosc="2,5l", Przebieg="240000km", RodzajPaliwa="Diesel", RokProdukcji=2007, ZdjecieUrl=""  },
+                new Samochod {Id=1, Cena=12500, JestSamochodemTygodnia=false, Marka="BMW", MiniaturkaUrl="/images/e46.jpg", Moc= "150KM", Model="E46", Opis="Niemiec płakał jak sprzedawał", Pojemnosc="2,5l", Przebieg="220000km", RodzajPaliwa="Benzyna", RokProdukcji=2002, ZdjecieUrl=""  },
+                new Samochod {Id=2, Cena=7500, JestSamochodemTygodnia=true, Marka="Ford", MiniaturkaUrl="/images/mondeo.jpg", Moc= "110KM", Model="Mondeo", Opis="Żabojad płakał jak sprzedawał", Pojemnosc="2,5l", Przebieg="180000km", RodzajPaliwa="Benzyna + Gaz", RokProdukcji=1999, ZdjecieUrl=""  },
+                new Samochod {Id=3, Cena=20000, JestSamochodemTygodnia=false, Marka="Mercedes", MiniaturkaUrl="/images/clk200.jpg", Moc= "205KM", Model="CLK200", Opis="Znowu Niemiec płakał jak sprzedawał", Pojemnosc="2,5l", Przebieg="240000km", RodzajPaliwa="Diesel", RokProdukcji=2007, ZdjecieUrl=""  },
+                new Samochod {Id=1, Cena=12500, JestSamochodemTygodnia=false, Marka="BMW", MiniaturkaUrl="/images/e46.jpg", Moc= "150KM", Model="E46", Opis="Niemiec płakał jak sprzedawał", Pojemnosc="2,5l", Przebieg="220000km", RodzajPaliwa="Benzyna", RokProdukcji=2002, ZdjecieUrl=""  },
+                new Samochod {Id=2, Cena=7500, JestSamochodemTygodnia=true, Marka="Ford", MiniaturkaUrl="/images/mondeo.jpg", Moc= "110KM", Model="Mondeo", Opis="Żabojad płakał jak sprzedawał", Pojemnosc="2,5l", Przebieg="180000km", RodzajPaliwa="Benzyna + Gaz", RokProdukcji=1999, ZdjecieUrl=""  },
+                new Samochod {Id=3, Cena=20000, JestSamochodemTygodnia=false, Marka="Mercedes", MiniaturkaUrl="/images/clk200.jpg", Moc= "205KM", Model="CLK200", Opis="Znowu Niemiec płakał jak sprzedawał", Pojemnosc="2,5l", Przebieg="240000km", RodzajPaliwa="Diesel", RokProdukcji=2007, ZdjecieUrl=""  }
             };
         }
 
