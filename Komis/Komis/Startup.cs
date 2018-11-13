a864b2351108625da1955f46c423573b88a225b2
Marcin Buczyński <buczynski.programuje@gmail.com>
2018-11-13 15:57:32 +0100

    implementacja entityframework i bazy danych

--- Komis/Komis/Startup.cs
+++ Komis/Komis/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using Komis.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Komis
@@ -8,11 +10,19 @@ namespace Komis
 
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<ISamochodRepository, MockSamochodRepository>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<ISamochodRepository, SamochodRepository>();
             services.AddMvc();
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
