2cb894f0d80c427ff1287b102e8c1d4f2ddff4f0
Marcin Buczyński <buczynski.programuje@gmail.com>
2018-11-24 07:25:01 +0100

    Add details view and nav

--- Komis/Komis/Models/DbInitializer.cs
+++ Komis/Komis/Models/DbInitializer.cs
@@ -13,11 +13,11 @@ namespace Komis.Models
             {
                 context.AddRange(
 
-                new Samochod { Cena = 12500, JestSamochodemTygodnia = false, Marka = "BMW", MiniaturkaUrl = "/images/e46.jpg", Moc = "150KM", Model = "E46", Opis = "Niemiec płakał jak sprzedawał", Pojemnosc = "2,5l", Przebieg = "220000km", RodzajPaliwa = "Benzyna", RokProdukcji = 2002, ZdjecieUrl = "" },
-                new Samochod { Cena = 7500, JestSamochodemTygodnia = true, Marka = "Ford", MiniaturkaUrl = "/images/mondeo.jpg", Moc = "110KM", Model = "Mondeo", Opis = "Żabojad płakał jak sprzedawał", Pojemnosc = "2,5l", Przebieg = "180000km", RodzajPaliwa = "Benzyna + Gaz", RokProdukcji = 1999, ZdjecieUrl = "" },
-                new Samochod { Cena = 20000, JestSamochodemTygodnia = false, Marka = "Mercedes", MiniaturkaUrl = "/images/clk200.jpg", Moc = "205KM", Model = "CLK200", Opis = "Znowu Niemiec płakał jak sprzedawał", Pojemnosc = "2,5l", Przebieg = "240000km", RodzajPaliwa = "Diesel", RokProdukcji = 2007, ZdjecieUrl = "" },
-                new Samochod { Cena = 9000, JestSamochodemTygodnia = false, Marka = "Peugeot", MiniaturkaUrl = "/images/e46.jpg", Moc = "133KM", Model = "406", Opis = "Marzy mi się to autko", Pojemnosc = "2,0l", Przebieg = "220000km", RodzajPaliwa = "Benzyna+Gaz", RokProdukcji = 1999, ZdjecieUrl = "" },
-                new Samochod { Cena = 1500, JestSamochodemTygodnia = false, Marka = "Ford", MiniaturkaUrl = "/images/mondeo.jpg", Moc = "110KM", Model = "Mondeo", Opis = "Żabojad płakał jak sprzedawał", Pojemnosc = "2,5l", Przebieg = "180000km", RodzajPaliwa = "Benzyna + Gaz", RokProdukcji = 1999, ZdjecieUrl = "" }
+                new Samochod { Cena = 12500, JestSamochodemTygodnia = false, Marka = "BMW", MiniaturkaUrl = "/images/e46.jpg", Moc = "150KM", Model = "E46", Opis = "Niemiec płakał jak sprzedawał", Pojemnosc = "2,5l", Przebieg = "220000km", RodzajPaliwa = "Benzyna", RokProdukcji = 2002, ZdjecieUrl = "/images/e46.jpg" },
+                new Samochod { Cena = 7500, JestSamochodemTygodnia = true, Marka = "Ford", MiniaturkaUrl = "/images/mondeo.jpg", Moc = "110KM", Model = "Mondeo", Opis = "Żabojad płakał jak sprzedawał", Pojemnosc = "2,5l", Przebieg = "180000km", RodzajPaliwa = "Benzyna + Gaz", RokProdukcji = 1999, ZdjecieUrl = "/images/mondeo.jpg" },
+                new Samochod { Cena = 20000, JestSamochodemTygodnia = false, Marka = "Mercedes", MiniaturkaUrl = "/images/clk200.jpg", Moc = "205KM", Model = "CLK200", Opis = "Znowu Niemiec płakał jak sprzedawał", Pojemnosc = "2,5l", Przebieg = "240000km", RodzajPaliwa = "Diesel", RokProdukcji = 2007, ZdjecieUrl = "/images/clk200.jpg" },
+                new Samochod { Cena = 9000, JestSamochodemTygodnia = false, Marka = "Peugeot", MiniaturkaUrl = "/images/e46.jpg", Moc = "133KM", Model = "406", Opis = "Marzy mi się to autko", Pojemnosc = "2,0l", Przebieg = "220000km", RodzajPaliwa = "Benzyna+Gaz", RokProdukcji = 1999, ZdjecieUrl = "/images/e46.jpg" },
+                new Samochod { Cena = 1500, JestSamochodemTygodnia = false, Marka = "Ford", MiniaturkaUrl = "/images/mondeo.jpg", Moc = "110KM", Model = "Mondeo", Opis = "Żabojad płakał jak sprzedawał", Pojemnosc = "2,5l", Przebieg = "180000km", RodzajPaliwa = "Benzyna + Gaz", RokProdukcji = 1999, ZdjecieUrl = "/images/mondeo.jpg" }
 
                     );
             }
