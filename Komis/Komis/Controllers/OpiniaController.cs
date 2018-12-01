4b5d3bdfd390ebc80a9db4501243e8bbc5bdbb53
Marcin Buczyński <buczynski.programuje@gmail.com>
2018-12-01 22:48:12 +0100

    Dodanie formularza opinii, widoku i modelu i kontrolera.

---
+++ Komis/Komis/Controllers/OpiniaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Komis.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Komis.Controllers
+{
+    public class OpiniaController : Controller
+    {
+        private readonly IOpiniaRepository _opiniaRepository;
+
+        public OpiniaController(IOpiniaRepository opiniaRepository)
+        {
+            _opiniaRepository = opiniaRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Opinia opinia)
+        {
+            _opiniaRepository.DodajOpinie(opinia);
+
+            return RedirectToAction("OpiniaWyslana");
+        }
+
+        public IActionResult OpiniaWyslana()
+        {
+            return View();
+        }
+    }
+}
