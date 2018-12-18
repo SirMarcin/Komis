95e73bff817278e1d9630bb93f959642cdb7ed72
Marcin Buczyński <buczynski.programuje@gmail.com>
2018-12-18 23:49:12 +0100

    CRUD

--- Komis/Komis/Controllers/SamochodController.cs
+++ Komis/Komis/Controllers/SamochodController.cs
@@ -61,6 +61,39 @@ namespace Komis.Controllers
 
             return View(samochod);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Samochod samochod)
+        {
+            if (ModelState.IsValid)
+            {
+                _samochodRepository.EdytujSamochod(samochod);
+                return RedirectToAction("Index");
+            }
+            return View(samochod);
+        }
+
+        public IActionResult Delete (int Id)
+        {
+            var samochod = _samochodRepository.PobierzSamochodId(Id);
+
+            if (samochod == null)
+            {
+                return NotFound();
+            }
+
+            return View(samochod);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int Id)
+        {
+            var samochod = _samochodRepository.PobierzSamochodId(Id);
+            _samochodRepository.UsunSamochod(samochod);
+            return RedirectToAction("Index");
+        }
     }
 }
  
